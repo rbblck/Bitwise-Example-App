9a039892e1522a68866a61be0590566190b9d882
robertblack <rbblck1969@gmail.com>
2022-03-05 11:21:14 +0000

    Update xcode project

---
+++ EngineBitwise/test.cpp
@@ -0,0 +1,8 @@
+//
+//  test.cpp
+//  EngineBitwise
+//
+//  Created by Robert Black on 05/03/2022.
+//
+
+#include "test.hpp"
---
+++ EngineBitwise/test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  EngineBitwise
+//
+//  Created by Robert Black on 05/03/2022.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
